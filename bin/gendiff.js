a5cbb8c13e898eca94f764ec43e18c3287737648
Denis <rainbowuran@gmail.com>
2023-04-15 17:58:48 +0300

    who

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
-import { Command } from 'commander/index.js';
+import { Command } from "commander";
+
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .version('1.0.0')
 
 program.parse();
